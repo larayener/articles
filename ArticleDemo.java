2d6bc5880bda856d99048a4c965186f4eec475a9
Lara Yener <45222107+larayener@users.noreply.github.com>
2019-09-20 22:26:57 -0400

    Create ArticleDemo.java

---
+++ ArticleDemo.java
@@ -0,0 +1,10 @@
+
+public class ArticleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArticleWordCounter o = new ArticleWordCounter();
+		o.wordCount("article.txt");
+	}
+
+}
